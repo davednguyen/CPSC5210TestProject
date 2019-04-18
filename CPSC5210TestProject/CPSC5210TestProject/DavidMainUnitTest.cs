af338efab081570614c0e6d4e192c6983cf3cb9e
davednguyen <davednguyen@gmail.com>
2019-04-18 10:26:11 -0700

    add more unit test cases to the solution

--- CPSC5210TestProject/CPSC5210TestProject/DavidMainUnitTest.cs
+++ CPSC5210TestProject/CPSC5210TestProject/DavidMainUnitTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PdfSharp;
 using PdfSharp.Pdf;
+using System.IO;
 
 namespace CPSC5210TestProject
 {
@@ -74,6 +75,21 @@ namespace CPSC5210TestProject
             PdfDocument document = new PdfDocument();
             PdfPage page = document.AddPage();
             Assert.IsNotNull(page);
+        }
+
+        [TestMethod]
+        public void CheckCreatePDFDocumentWithName()
+        {
+            PdfDocument testDocument = new PdfDocument("TestFile");
+            Assert.IsNotNull(testDocument);
+        }
+
+        [TestMethod]
+        public void CheckCreatePDFDocumentWithStream()
+        {
+            Stream outputStream=null;
+            PdfDocument testDocument = new PdfDocument(outputStream);
+            Assert.IsNotNull(testDocument);
         }
     }
 }
