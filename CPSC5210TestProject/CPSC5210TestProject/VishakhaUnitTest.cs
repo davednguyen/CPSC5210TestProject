e99ae9a604b90c5b8455333dbd9b20eca1b89f24
Vishakha Bhavsar <vishakhabhavsar@Vishakhas-MacBook-Pro.local>
2019-05-02 09:55:03 -0700

    Added my test cases

--- CPSC5210TestProject/CPSC5210TestProject/VishakhaUnitTest.cs
+++ CPSC5210TestProject/CPSC5210TestProject/VishakhaUnitTest.cs
@@ -6,9 +6,28 @@ namespace CPSC5210TestProject
     [TestClass]
     public class VishakhaUnitTest
     {
-        [TestMethod]
-        public void FirstTest()
+        [Test()]
+        public void TestPdfDocumentToBeNotNull()
         {
+            PdfDocument document = new PdfDocument();
+            Assert.NotNull(document);
+        }
+
+        [Test()]
+        public void TestPageCountMatchesAddedPages()
+        {
+            PdfDocument document = new PdfDocument();
+            document.AddPage();
+            document.AddPage();
+            Assert.True(2 == document.PageCount);
+
+        }
+        [Test()]
+        public void TestTitleMatches()
+        {
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+            Assert.Equals(document.Info.Title,"Created with PDFsharp");
         }
     }
 }
